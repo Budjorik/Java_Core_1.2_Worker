594473179c28d2bef6b8f85c4cba7615cf7df346
Сергей Караульных <karaulnih@gmail.com>
2022-02-27 17:59:44 +0500

    Create files

---
+++ Main.java
@@ -0,0 +1,15 @@
+package ru.netology.lesson1;
+
+import java.util.function.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener errorListener = System.out::println;
+
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+
+    }
+}
